d606bc6548d0b096fb7ca39b3149b530785cb17c
Pavel <gpg@ua.fm>
2017-07-22 15:55:54 +0300

    beta version, pagination

--- src/main/java/com/example/demo/controller/ContactController.java
+++ src/main/java/com/example/demo/controller/ContactController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import com.example.demo.model.Contact;
 import com.example.demo.service.ContactService;
+import com.example.demo.to.ContactTo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,6 +25,12 @@ public class ContactController {
     @Autowired
     private ContactService service;
 
+    @Autowired
+    private ContactTo contactTo;
+
+    private String regexInit = "";
+    private List<Contact> contacts;
+
     @GetMapping
     public List<Contact> getAll(@RequestParam(value = "nameFilter", defaultValue = "") String regex) {
         List<Contact> contacts = service.getAll(regex);
@@ -31,10 +38,50 @@ public class ContactController {
         return contacts;
     }
 
+    @GetMapping("/pages")
+    public ContactTo getPage(
+            @RequestParam(value = "nameFilter", defaultValue = "") String regex,
+            @RequestParam("page") int page,
+            @RequestParam(value = "cnt", required = false, defaultValue = "500_000") int pageSize) {
+
+        if (!regexInit.equals(regex)) {
+            contacts = service.getAll(regex);
+            regexInit = regex;
+            Collections.sort(contacts, Comparator.comparingLong(o -> o.getId()));
+        }
+        return getPage(contacts, page, pageSize);
+    }
+
     @GetMapping("/standard")
     public List<Contact> getAllStandard(@RequestParam(value = "nameFilter", defaultValue = "") String regex) {
         List<Contact> contacts = service.getAllStandard(regex);
         Collections.sort(contacts, Comparator.comparingLong(o -> o.getId()));
         return contacts;
+    }
+
+    /*
+     * returns a view (not a new list) of the sourceList for the
+     * range based on page and pageSize
+     * @param sourceList
+     * @param page
+     * @param pageSize
+     * @return
+     */
+    public ContactTo getPage(List<Contact> sourceList, int page, int pageSize) {
+        if (pageSize <= 0 || page <= 0) {
+            throw new IllegalArgumentException("invalid page size: " + pageSize);
+        }
+
+        int fromIndex = (page - 1) * pageSize;
+        if (sourceList == null || sourceList.size() < fromIndex) {
+            return contactTo;
+        }
+
+        // toIndex exclusive
+        contactTo.setContacts(sourceList.subList(fromIndex, Math.min(fromIndex + pageSize, sourceList.size())));
+        contactTo.setCurrentPage(page);
+        contactTo.setTotalCount(sourceList.size());
+        contactTo.setTotalPages((sourceList.size()/pageSize)+1);
+        return contactTo;
     }
 }
--- src/main/java/com/example/demo/to/ContactTo.java
+++ src/main/java/com/example/demo/to/ContactTo.java
@@ -3,13 +3,15 @@ package com.example.demo.to;
 import com.example.demo.model.Contact;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 @Data
 @NoArgsConstructor
+@Component
 public class ContactTo {
     private List<Contact> contacts;
     private long totalCount;
     private Integer currentPage;
-    private Integer totalPage;
+    private Integer totalPages;
 }
