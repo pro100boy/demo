84670db9e3c39101fe9ad44e554573d6a87edb89
Pavel <gpg@ua.fm>
2017-07-23 12:54:38 +0300

    RC1

--- src/main/java/com/example/demo/service/ContactServiceImpl.java
+++ src/main/java/com/example/demo/service/ContactServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.demo.service;
 import com.example.demo.model.Contact;
 import com.example.demo.repository.ContactRepository;
 import lombok.NonNull;
+import lombok.Setter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,22 +16,25 @@ import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 @Service
 public class ContactServiceImpl implements ContactService {
     private final Logger log = LoggerFactory.getLogger(getClass());
+    private CopyOnWriteArrayList<Contact> commonContactList = new CopyOnWriteArrayList<>();
 
     @Autowired
     private ContactRepository repository;
 
-    @Autowired
-    private FilterService filterService;
+    @Setter
+    private Pattern pattern;
 
     @Override
     public List<Contact> getAll(@NonNull String regex) {
         log.info(regex.isEmpty() ? "getAll" : "getAll with regex: " + regex);
         getDataParallel(regex);
-        log.info(String.format("Returned %d objects", commonContactList.size()));
+        log.info(String.format("Selecting and sorting are completed. Returned %d objects", commonContactList.size()));
         return commonContactList;
     }
 
@@ -44,10 +48,13 @@ public class ContactServiceImpl implements ContactService {
         int nThreads = 5;//Runtime.getRuntime().availableProcessors();
         ExecutorService executor = Executors.newFixedThreadPool(nThreads);
         CountDownLatch latch = new CountDownLatch(nThreads);
+        setPattern(Pattern.compile(regex));
+        long recTotal = repository.count();
+        long partSize = recTotal % nThreads == 0 ? recTotal / nThreads : recTotal / nThreads + 1;
         commonContactList.clear();
 
         for (int i = 0; i < nThreads; i++) {
-            executor.submit(new DBSelector(latch, i, nThreads, regex));
+            executor.submit(new DBSelector(latch, i, nThreads, partSize));
         }
 
         try {
@@ -56,45 +63,39 @@ public class ContactServiceImpl implements ContactService {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-
-        log.info("Selecting and sorting are completed");
     }
 
     class DBSelector implements Runnable {
         private CountDownLatch latch;
         private int i;
         private int nThreads;
-        private String regex;
-        private long size;
+        private long partSize;
 
-        public DBSelector(CountDownLatch latch, int i, int nThreads, String regex) {
+        public DBSelector(CountDownLatch latch, int i, int nThreads, long partSize) {
             this.latch = latch;
             this.i = i;
             this.nThreads = nThreads;
-            this.regex = regex;
-            size = repository.count() % nThreads == 0 ? repository.count() / nThreads : (repository.count() / nThreads) + 1;
+            this.partSize = partSize;
         }
 
         @Override
         public void run() {
             // select portion of the data
-            log.info(String.format("Start selecting portion %d of %d", i+1, nThreads));
+            log.info(String.format("Start selecting portion %d of %d", i + 1, nThreads));
             try {
-                Pageable pageable = createPageRequest();
+                Pageable pageable = new PageRequest(i, (int)partSize);
                 List<Contact> contactPageList = repository.findAll(pageable).getContent();
-                commonContactList.addAll(filterService.doFilter(contactPageList, regex));
+                commonContactList.addAll(doFilter(contactPageList));
             } finally {
                 latch.countDown();
-                log.info(String.format("Finished selecting and sorting portion %d of %d", i+1, nThreads));
+                log.info(String.format("Finished selecting and sorting portion %d of %d", i + 1, nThreads));
             }
         }
 
-        private Pageable createPageRequest() {
-            // TODO определять автоматич. size
-            return new PageRequest(i, (int)size);
+        private List<Contact> doFilter(List<Contact> contacts) {
+            return contacts.parallelStream()
+                    .filter(s -> !pattern.matcher(s.getName()).matches())
+                    .collect(Collectors.toList());
         }
     }
-
-    private CopyOnWriteArrayList<Contact> commonContactList = new CopyOnWriteArrayList<>();
-
 }
--- src/main/java/com/example/demo/service/FilterService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.model.Contact;
-
-import java.util.List;
-
-public interface FilterService {
-    List<Contact> doFilter(List<Contact> contacts, String regexp);
-}
